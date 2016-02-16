f962cd416eaa7b67dfc03131134e5e102d4177d6
Cătălin Mariș <alrraa@gmail.com>
2016-02-16 15:48:27 +0200

    Move helper functions in `bin/util/misc.sh`

--- bin/commit-changes.sh
+++ bin/commit-changes.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-declare -r LOG_PREFIX='[travis-scripts]'
+# Load helper functions
+source "$(
+
+    # The following is done because:
+    #
+    #   * `readlink` on OS X outputs a relative path
+    #   * `misc.sh` is not publicly exposed
+    #
+
+    cd "$(dirname "$BASH_SOURCE")";
+    cd "$(dirname $(readlink "$BASH_SOURCE"))";
+    pwd
+
+)/util/misc.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -20,24 +33,6 @@ commit_and_push_changes() {
 
 }
 
-execute() {
-    eval ${1}
-}
-
-get_repository_url() {
-
-    if [ -z "$GH_TOKEN" ]; then
-        printf "git@github.com:$TRAVIS_REPO_SLUG.git"
-    else
-        printf "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-    fi
-
-}
-
-print_error() {
-    print_in_red "$LOG_PREFIX [✖] $1 $2\n"
-}
-
 print_help_message() {
     printf '\n'
     printf 'OPTIONS:'
@@ -55,42 +50,6 @@ print_help_message() {
     printf '\n'
     printf '     Specifies the commit message\n'
     printf '\n'
-}
-
-print_in_green() {
-    printf "\e[0;32m$1\e[0m"
-}
-
-print_in_red() {
-    printf "\e[0;31m$1\e[0m"
-}
-
-print_result() {
-    [ $1 -eq 0 ] \
-        && print_success "$2" \
-        || print_error "$2"
-
-    return $1
-}
-
-print_success() {
-    print_in_green "$LOG_PREFIX [✔] $1\n"
-}
-
-remove_sensitive_information() {
-
-    declare -r CENSURE_TEST='[secure]';
-
-    while read line; do
-
-        line="${line//${GH_TOKEN}/$CENSURE_TEST}"
-        line="${line//${GH_USER_EMAIL}/$CENSURE_TEST}"
-        line="${line//${GH_USER_NAME}/$CENSURE_TEST}"
-
-        print_error "$line"
-
-    done
-
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- bin/update-branch.sh
+++ bin/update-branch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-declare -r LOG_PREFIX='[travis-scripts]'
+# Load helper functions
+source "$(
+
+    # The following is done because:
+    #
+    #   * `readlink` on OS X outputs a relative path
+    #   * `misc.sh` is not publicly exposed
+    #
+
+    cd "$(dirname "$BASH_SOURCE")";
+    cd "$(dirname $(readlink "$BASH_SOURCE"))";
+    pwd
+
+)/util/misc.sh"
+
 declare repository_url=''
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -18,24 +32,6 @@ commit_and_push_changes() {
         && git checkout --quiet -b "$1" \
         && git push --quiet --force "$repository_url" "$1"
 
-}
-
-execute() {
-    eval ${1}
-}
-
-get_repository_url() {
-
-    if [ -z "$GH_TOKEN" ]; then
-        printf "git@github.com:$TRAVIS_REPO_SLUG.git"
-    else
-        printf "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-    fi
-
-}
-
-print_error() {
-    print_in_red "$LOG_PREFIX [✖] $1 $2\n"
 }
 
 print_help_message() {
@@ -63,42 +59,6 @@ print_help_message() {
     printf '\n'
     printf '     Specifies the name of the branch that contains the source code\n'
     printf '\n'
-}
-
-print_in_green() {
-    printf "\e[0;32m$1\e[0m"
-}
-
-print_in_red() {
-    printf "\e[0;31m$1\e[0m"
-}
-
-print_result() {
-    [ $1 -eq 0 ] \
-        && print_success "$2" \
-        || print_error "$2"
-
-    return $1
-}
-
-print_success() {
-    print_in_green "$LOG_PREFIX [✔] $1\n"
-}
-
-remove_sensitive_information() {
-
-    declare -r CENSURE_TEST='[secure]';
-
-    while read line; do
-
-        line="${line//${GH_TOKEN}/$CENSURE_TEST}"
-        line="${line//${GH_USER_EMAIL}/$CENSURE_TEST}"
-        line="${line//${GH_USER_NAME}/$CENSURE_TEST}"
-
-        print_error "$line"
-
-    done
-
 }
 
 remove_unneeded_files() {
